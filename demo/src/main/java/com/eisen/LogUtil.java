cb5f7c9fceb9e4f326b10051e83515fd98487282
Eisen <eisen7@yeah.nt>
2018-06-28 18:19:49 +0800

    new

---
+++ demo/src/main/java/com/eisen/LogUtil.java
@@ -0,0 +1,17 @@
+package com.eisen;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * @Author 解玉新
+ * @Date 17:04 2018/6/28
+ */
+public class LogUtil {
+    public static Logger logger = LoggerFactory.getLogger(LogUtil.class);
+
+    public static void main(String[] args) {
+        logger.info("test");
+    }
+}
